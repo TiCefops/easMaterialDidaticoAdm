6ca36f976d0c722dfca243ef80e03a32949428f1
e.dias <ti@cefops.com.br>
2022-11-28 16:45:51 -0300

    foi feita a refatoração do codigo removendo a dependencia do firebaseFirestore e foi necessario reconfigurar a parte de teste

--- test/infra/repositories/course_repository_impl_test.dart
+++ test/infra/repositories/course_repository_impl_test.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easmaterialdidaticoadm/domain/entities/course_entity.dart';
 import 'package:easmaterialdidaticoadm/domain/repositories/course_repository.dart';
-import 'package:easmaterialdidaticoadm/external/datasources/get_course_by_id_provider.dart';
+import 'package:easmaterialdidaticoadm/external/datasources/get_course_datasource_impl.dart';
 import 'package:easmaterialdidaticoadm/infra/repositories/course_repository_impl.dart';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -9,19 +9,21 @@ import '../../mocks/FirestoreMock.dart';
 
 void main() async {
   final FirestoreMock _mock = FirestoreMock();
-  final coureDb=CourseDataSourceImpl(_mock.setCourseData());
+  final coureDb = CourseDataSourceImpl(_mock.setCourseData());
   final CourseRepository _repository = CourseRepositoryImpl(coureDb);
 
   group("Course Repository test", () {
     test("getCourseById", () async {
-      DocumentSnapshot course =
+      CourseEntity course =
           await _repository.getCourseById(id: "JKvhYXcuDsUhKrgbvYP7");
-      expect(true, course.exists);
-      expect("Auxiliar de Enfermagem", course.get("nome"));
+      expect(true, course.ativo);
+      expect("Auxiliar de Enfermagem", course.nome);
     });
 
-    test("getAllCourses", () async* {
-      Stream<QuerySnapshot> course = _repository.getAllCourse();
+    test("getAllCourses", () async {
+      List<CourseEntity> course = await _repository.getAllCourse();
+      expect(2, course.length);
+      expect("Auxiliar de Enfermagem", course.first.nome);
 
     });
   });
