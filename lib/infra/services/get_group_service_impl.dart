2b9556b2b6ba7e806c7abde12dbd6f7aa2139d4a
e.dias <ti@cefops.com.br>
2022-11-28 16:47:24 -0300

    removido depencencia do firebaseFirestore

--- lib/infra/services/get_group_service_impl.dart
+++ lib/infra/services/get_group_service_impl.dart
@@ -1,15 +1,15 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easmaterialdidaticoadm/domain/usercases/get_group_service.dart';
-import 'package:easmaterialdidaticoadm/external/datasources/get_course_by_id_provider.dart';
-import 'package:easmaterialdidaticoadm/infra/datasources/course_datasource.dart';
+import '../../domain/entities/course_entity.dart';
 import '../../domain/repositories/course_repository.dart';
+import '../../domain/usercases/group/get_group_service.dart';
+import '../../external/datasources/get_course_datasource_impl.dart';
 import '../repositories/course_repository_impl.dart';
 
 class GetGroupServiceImpl implements GetGroupService {
 
   @override
-  Future<DocumentSnapshot> getGroup(String id)async{
+  Future<CourseEntity> getGroup(String id)async{
     final FirebaseFirestore firestore=FirebaseFirestore.instance;
     final courseDataSource=CourseDataSourceImpl(firestore);
     CourseRepository _repository=CourseRepositoryImpl(courseDataSource);
